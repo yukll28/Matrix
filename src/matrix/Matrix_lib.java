abb5f114e6343998e7ea679224969b769d57977f
50616021 <50616021@50616021-pc>
2017-11-06 17:29:48 +0900

    余因子行列

--- src/matrix/Matrix_lib.java
+++ src/matrix/Matrix_lib.java
@@ -1,15 +1,10 @@
 package matrix;
 
-public class Matrix_lib {
-	
-	private double a[][],b[][];
-	
-	public Matrix_lib(double[][] a, double[][] b) {
-		this.a = a;
-		this.b = b;
-	}
+import java.util.Arrays;
 
-   public double [][] getProduct(){
+public class Matrix_lib {
+
+   public double [][] getProduct(double a[][],double b [][]){
 	   double answer[][] = new double[a.length][b[0].length];
 	   double t[][] = this.getTranspose(b);
 	   for(int i = 0; i < a.length; i++) {
@@ -40,5 +35,64 @@ public class Matrix_lib {
 		return t;
 	}
 	
+	public double getCofactor(double [][] a) {
+		double cofactor=0;
+		if(a.length == 2) {
+			cofactor = a[0][0]*a[1][1]-a[0][1]*a[1][0];
+		}
+		else {
+			double tmp[][]=new double[a.length-1][a[0].length-1];
+			int index = 0;
+			
+			for(int i = 0; i < a.length; i++ ) {
+				 int p = 0, q = 0;
+				 for(int j = 0; j < a.length; j++) {
+				     if( i == j ) continue;
+				     for(int k = 0; k < a[0].length; k++) {
+				        if( index == k ) continue;
+				        tmp[p][q++] = a[j][k];
+				        }
+				        p++;
+				        q = 0;
+				   }
+		   System.out.println("tmp["+i+"]=" + Arrays.deepToString(tmp));
+			cofactor += a[i][index] * Math.pow(-1, ( i + 1 ) + (index + 1 )) * this.getCofactor(tmp);
+
+			
+		}
+		}
+		
+		return cofactor;
+	
+
+   }
+	
+	
+	public double [][] getInverse(double[][] a){
+		double answer [][] = new double [a.length][a[0].length];
+		double tmp[][] = new double[a.length -1][a[0].length -1];
+        double determinant = this.getCofactor(a);
+        
+        for(int i = 0; i < a.length; i++) {
+           for(int j = 0; j < a[0].length; j++) {
+           int p = 0, q = 0;
+           for(int k = 0; k < a.length; k++) {
+           if( i == k ) continue;
+           for(int s = 0; s < a[0].length; s++) {
+           if( j == s ) continue;
+        	 tmp[p][q] = a[k][s];
+        	q++;
+        	}
+        	 p++;
+        	 q = 0;
+        	}
+        	answer[i][j] = Math.pow(-1, i + 1 + j + 1) * this.getCofactor(tmp) / determinant;
+           }
+        }
+
+        answer = this.getTranspose(answer);
+		return answer;
+		}
 
 }
+
--- src/matrix/Matrix_main.java
+++ src/matrix/Matrix_main.java
@@ -6,11 +6,17 @@ public class Matrix_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      double a[][]= {{0,1},{2,0},{0,3}};
-      double b[][]= {{1,-1,2},{3,0,4}};
-      Matrix_lib mlib = new Matrix_lib(a,b);
-      System.out.println("Product="+Arrays.deepToString(mlib.getProduct()));
-      System.out.println("Transpose="+Arrays.deepToString(mlib.getTranspose(b)));
+      double a[][]= {{1,2},{-1,1}};
+      double b[][]= {{3,1},{2,1}};
+      
+      double c[][] = { { 3, 1, 1, 2 },{ 5, 1, 3, 4 },{ 2, 0, 1, 0 },{ 1, 3, 2, 1 }};
+
+      Matrix_lib mlib = new Matrix_lib();
+      System.out.println("Product="+Arrays.deepToString(mlib.getProduct(a,b)));
+      System.out.println("Cofactor = "+ (mlib.getCofactor(c)));
+      System.out.println("Inverse = "+ Arrays.deepToString(mlib.getInverse(c)));
+
+
 	}   
 
 }
